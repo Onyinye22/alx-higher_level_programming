89efaee496a8faa31e28d964de51ad4a765c08a8
Onyinye22 <ijeonyinye22@gmail.com>
2023-03-29 15:33:20 -0700

    square 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-"""Define a class Square."""
-
-
 class Square:
-    """Represent a square."""
-
+    """
+    creates a square class object
+    """
     def __init__(self, size):
-        """Initialize a new Square.
-
-        Args:
-            size (int): The size of the new square.
-        """
         self.__size = size
-
+    """
+    initializes a square object with parameters
+    args:
+        __size(int): size of the square
+    """
